8aca8ce16cdfb72dd73da54ad7634902e9308514
Juha Paananen <juha.paananen@gmail.com>
2015-02-20 22:40:49 +0200

    stats

--- server.js
+++ server.js
@@ -102,6 +102,7 @@ app.use(express.json())
 app.use('/', express.static(__dirname + '/public'))
 app.use('/', express.static(__dirname + '/node_modules/baconjs/dist'))
 app.use('/', express.static(__dirname + '/node_modules/jquery.cookie'))
+app.use('/', express.static(__dirname + '/node_modules/chart.js'))
 http.listen(port, function() {
   console.log("Vedä leukoi! " + port)
 })
