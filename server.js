32112a5656a50e528487a27b57508da3f3b5a04e
Juha Paananen <juha.paananen@gmail.com>
2015-07-27 10:40:25 +0300

    keep summary in property to avoid extra calls

--- server.js
+++ server.js
@@ -12,144 +12,149 @@ var mongoUrl = process.env["MONGOLAB_URI"] || "mongodb://localhost/leuat"
 var leuat
 var leuatBus = new Bacon.Bus()
 
-MongoClient.connect(mongoUrl, function(err, conn) {
-  if (err) {
+var connE = Bacon.fromNodeCallback(MongoClient.connect, mongoUrl)
+connE.onError(function(err) {
     console.log("Failed to connect to mongo", err)
     throw err
-  }
+})
+connE.onValue(function(conn) {
+  console.log("Connected to MongoDB")
+
   leuat = conn.collection("leuat")
-})
-
-function summary() {
-  console.log("calculate summary")
-  var sevenDays = 7 * 24 * 60 * 60 * 1000;
-  return Bacon.fromNodeCallback(
-    leuat, "aggregate", [{
-          $project: {
-            _id: 0,
-            team: 1,
-            leukoja: 1,
-            last: {$cond: [
-              {$gt: ["$date", new Date(new Date().getTime() - sevenDays)]},
-              "$leukoja",
-              0
-            ]}
-          }
-        }, {
-          $group: {
-            _id: "$team",
-            leukoja: {$sum: "$leukoja"},
-            muutos: {$sum: "$last"}
-          }
-        }]
-  ).map(function(list) {
-    return list
-      .sort(function(a,b) {
-        var muutos = b.muutos - a.muutos;
-        return muutos != 0 ? muutos : b.leukoja-a.leukoja;
-      })
-      .map(function(a) { return { team: a._id, leukoja: a.leukoja, muutos: a.muutos }})
-  })
-}
-
-function statsE(queryParam, groups, start, end) {
-  var groupBy = null
-  var query = {}
-  if (queryParam) {
-    groupBy = "$" + queryParam
-    query[queryParam] =  {$in: groups}
-  }
-  return Bacon.fromNodeCallback(leuat, "aggregate", { 
-    $match: _.extend({}, query, dateBetweenQuery(start, end))
-  },
-  { $group: { _id : groupBy, sum : { $sum: "$leukoja" } } })
-}
-
-function multiStats(queryParam, groups, interval, count) {
-  var statArr = []
-  var diff = 0
-  for (var i = 0; i < count; i++) {
-    statArr.push(statsE(queryParam, groups, diff - interval + 1, diff + 1))
-    diff -= interval
-  }
-  statArr = statArr.reverse()
-  return Bacon.combineTemplate(statArr)
-    .map(function(statsByDate) {
-      var groupedResults = {}
-      statsByDate.forEach(function(dayStats) {
-        var groupedDayStats = {}
-        dayStats.forEach(function(groupStatsForDay) {
-          groupedDayStats[groupStatsForDay._id] = groupStatsForDay.sum
+  function summary() {
+    console.log("calculate summary")
+    var sevenDays = 7 * 24 * 60 * 60 * 1000;
+    return Bacon.fromNodeCallback(
+      leuat, "aggregate", [{
+            $project: {
+              _id: 0,
+              team: 1,
+              leukoja: 1,
+              last: {$cond: [
+                {$gt: ["$date", new Date(new Date().getTime() - sevenDays)]},
+                "$leukoja",
+                0
+              ]}
+            }
+          }, {
+            $group: {
+              _id: "$team",
+              leukoja: {$sum: "$leukoja"},
+              muutos: {$sum: "$last"}
+            }
+          }]
+    ).map(function(list) {
+      return list
+        .sort(function(a,b) {
+          var muutos = b.muutos - a.muutos;
+          return muutos != 0 ? muutos : b.leukoja-a.leukoja;
         })
-        groups.forEach(function(groupId) {
-          groupedResults[groupId] = (groupedResults[groupId] || []).concat(groupedDayStats[groupId] || 0)
-        })
-      })
-      return groupedResults
+        .map(function(a) { return { team: a._id, leukoja: a.leukoja, muutos: a.muutos }})
     })
-}
-
-function dateBetweenQuery(start, end) {
-  return { $and: [
-    { date: { $gte: dateStart(start) } },
-    { date: { $lte: dateStart(end) } }
-  ]}
-}
-
-function dateStart(diff) {
-  var d = new Date(new Date().getTime() + 86400000 * diff)
-  d.setHours(0,0,0,0)
-  return d
-}
-
-var statusUpdateE = leuatBus.flatMap(summary)
-
-io.on('connection', function(socket){
-  console.log("socket connected")
-  var discoE = Bacon.fromEvent(socket, "disconnect")
-  socket.on("leuka", function(msg) {
-    leuat.insert({team: msg.team, leukoja: msg.leukoja, vetaja: msg.vetaja, date: new Date()})
-    leuatBus.push()
-  })
-  Bacon.fromEvent(socket, "leuat")
-    .takeUntil(discoE)
-    .map()
-    .log("leuat requested")
-    .flatMap(summary)
-    .onValue(send)
-  statusUpdateE.takeUntil(discoE).onValue(send)
-  discoE.log("socket disconnect")
-  function send(data) {
-    console.log("sending leuat")
-    socket.emit("leuat", data)
   }
-})
 
-function serveStats(stats, res) {
-  stats.onValue(function(data) {
-    res.end(JSON.stringify(data))
+  function statsE(queryParam, groups, start, end) {
+    var groupBy = null
+    var query = {}
+    if (queryParam) {
+      groupBy = "$" + queryParam
+      query[queryParam] =  {$in: groups}
+    }
+    return Bacon.fromNodeCallback(leuat, "aggregate", { 
+      $match: _.extend({}, query, dateBetweenQuery(start, end))
+    },
+    { $group: { _id : groupBy, sum : { $sum: "$leukoja" } } })
+  }
+
+  function multiStats(queryParam, groups, interval, count) {
+    var statArr = []
+    var diff = 0
+    for (var i = 0; i < count; i++) {
+      statArr.push(statsE(queryParam, groups, diff - interval + 1, diff + 1))
+      diff -= interval
+    }
+    statArr = statArr.reverse()
+    return Bacon.combineTemplate(statArr)
+      .map(function(statsByDate) {
+        var groupedResults = {}
+        statsByDate.forEach(function(dayStats) {
+          var groupedDayStats = {}
+          dayStats.forEach(function(groupStatsForDay) {
+            groupedDayStats[groupStatsForDay._id] = groupStatsForDay.sum
+          })
+          groups.forEach(function(groupId) {
+            groupedResults[groupId] = (groupedResults[groupId] || []).concat(groupedDayStats[groupId] || 0)
+          })
+        })
+        return groupedResults
+      })
+  }
+
+  function dateBetweenQuery(start, end) {
+    return { $and: [
+      { date: { $gte: dateStart(start) } },
+      { date: { $lte: dateStart(end) } }
+    ]}
+  }
+
+  function dateStart(diff) {
+    var d = new Date(new Date().getTime() + 86400000 * diff)
+    d.setHours(0,0,0,0)
+    return d
+  }
+
+  var statusUpdateE = leuatBus.flatMap(summary)
+  var currentSummary = summary().concat(statusUpdateE).toProperty()
+  currentSummary.map("summary updated").log()
+
+  io.on('connection', function(socket){
+    console.log("socket connected")
+    var discoE = Bacon.fromEvent(socket, "disconnect")
+    socket.on("leuka", function(msg) {
+      leuat.insert({team: msg.team, leukoja: msg.leukoja, vetaja: msg.vetaja, date: new Date()})
+      leuatBus.push()
+    })
+    Bacon.fromEvent(socket, "leuat")
+      .takeUntil(discoE)
+      .map("leuat requested")
+      .log()
+      .flatMap(currentSummary.take(1))
+      .onValue(send)
+    statusUpdateE.takeUntil(discoE).onValue(send)
+    discoE.log("socket disconnect")
+    function send(data) {
+      console.log("sending leuat")
+      socket.emit("leuat", data)
+    }
   })
-}
-/*
-app.get("/leuat/vetaja/:vetaja/:interval/:count", function(req, res) {
-  serveStats(multiStats("vetaja", req.params.vetaja.split(","), req.params.interval, req.params.count), res)
+
+  function serveStats(stats, res) {
+    stats.onValue(function(data) {
+      res.end(JSON.stringify(data))
+    })
+  }
+  /*
+  app.get("/leuat/vetaja/:vetaja/:interval/:count", function(req, res) {
+    serveStats(multiStats("vetaja", req.params.vetaja.split(","), req.params.interval, req.params.count), res)
+  })
+
+  app.get("/leuat/team/:team/:interval/:count", function(req, res) {
+    serveStats(multiStats("team", req.params.team.split(","), req.params.interval, req.params.count), res)
+  })
+
+  app.get("/leuat/all/:interval/:count", function(req, res) {
+    serveStats(multiStats(null, [null], req.params.interval, req.params.count), res)
+  })
+  */
+
+  app.use(express.compress())
+  app.use(express.json())
+  app.use('/', express.static(__dirname + '/public'))
+  app.use('/', express.static(__dirname + '/node_modules/baconjs/dist'))
+  app.use('/', express.static(__dirname + '/node_modules/jquery.cookie'))
+  app.use('/', express.static(__dirname + '/node_modules/chart.js'))
+  http.listen(port, function() {
+    console.log("Vedä leukoi! " + port)
+  })
 })
 
-app.get("/leuat/team/:team/:interval/:count", function(req, res) {
-  serveStats(multiStats("team", req.params.team.split(","), req.params.interval, req.params.count), res)
-})
-
-app.get("/leuat/all/:interval/:count", function(req, res) {
-  serveStats(multiStats(null, [null], req.params.interval, req.params.count), res)
-})
-*/
-
-app.use(express.compress())
-app.use(express.json())
-app.use('/', express.static(__dirname + '/public'))
-app.use('/', express.static(__dirname + '/node_modules/baconjs/dist'))
-app.use('/', express.static(__dirname + '/node_modules/jquery.cookie'))
-app.use('/', express.static(__dirname + '/node_modules/chart.js'))
-http.listen(port, function() {
-  console.log("Vedä leukoi! " + port)
-})
